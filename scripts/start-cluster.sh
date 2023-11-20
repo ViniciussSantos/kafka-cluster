6052b39bfebd246e274a89a6c5102d00b8d28203
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-11-19 22:17:00 -0300

    Add script to start cluster

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-minikube_status=$(minikube status -b=0 | grep -o "type: Control Plane host: Running kubelet: Running apiserver: Running kubeconfig: Configured")
-
-if [ -z "$minikube_status" ]; then
-    echo "Starting Minikube..."
-    minikube start
-fi
+echo "fixing flannel network..."
+kubectl apply -f config/flannel.yaml >/dev/null 2>&1 
 
 echo "Applying Kubernetes configuration files..."
-kubectl apply -f ./config/00-namespace.yaml -n default
-kubectl apply -f ./config/01-kafka.yaml
-
-echo "Waiting for the Kafka cluster to be ready..."
-kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s -n default
-
-node_port=$(kubectl get service my-cluster-kafka-external-bootstrap -o=jsonpath='{.spec.ports[0].nodePort}')
-internal_ip=$(kubectl get node minikube -o=jsonpath='{.status.addresses[?(@.type=="InternalIP")].address}')
-
-echo "You can access the Kafka cluster at $internal_ip:$node_port"
+kubectl create namespace kafka >/dev/null 2>&1
+kubectl apply -f ./config/namespace.yaml -n kafka >/dev/null 2>&1
+kubectl apply -f ./config/kafka.yaml -n kafka >/dev/null 2>&1
