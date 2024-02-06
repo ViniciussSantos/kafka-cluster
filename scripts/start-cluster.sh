8dddfb842038fe20f490edba045162a2a3ec8875
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-02-06 10:29:58 -0300

    add more commands to start script

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 echo "fixing flannel network..."
-kubectl apply -f config/flannel.yaml >/dev/null 2>&1 
-
-echo "Applying Kubernetes configuration files..."
+kubectl apply -f config/flannel.yaml >/dev/null 2>&1
+echo "Creating kafka namespace"
 kubectl create namespace kafka >/dev/null 2>&1
+echo "creating kafka cluster"
 kubectl apply -f ./config/namespace.yaml -n kafka >/dev/null 2>&1
 kubectl apply -f ./config/kafka.yaml -n kafka >/dev/null 2>&1
+echo "creating kafka topics"
+kubectl apply -f ./config/topic.yaml -n kafka >/dev/null 2>&1
+echo "deploying kafka connect"
+kubectl apply -f ./config/connect.yaml -n kafka >/dev/null 2>&1
+echo "deploying kafka connect nodeport service"
+kubectl apply -f ./config/connect-port.yaml -n kafka >/dev/null 2>&1
