50ea4b16fac1bde0f66aa3f5b0123ff3fe63f9e3
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-02-07 00:36:32 -0300

    modify script to way for the kafka pods to be ready

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -7,6 +7,8 @@ kubectl create namespace kafka >/dev/null 2>&1
 echo "creating kafka cluster"
 kubectl apply -f ./config/namespace.yaml -n kafka >/dev/null 2>&1
 kubectl apply -f ./config/kafka.yaml -n kafka >/dev/null 2>&1
+echo "waiting for kafka to be ready"
+kubectl wait --for=condition=ready pod -l app=kafka -n kafka --timeout=300s
 echo "creating kafka topics"
 kubectl apply -f ./config/topic.yaml -n kafka >/dev/null 2>&1
 echo "deploying kafka connect"
