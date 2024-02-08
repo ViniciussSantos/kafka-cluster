b4f31a5f89835c0f21dca4145468d937fdbb8ed9
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-02-08 03:21:57 -0300

    fixed kubectl wait commands

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -6,9 +6,13 @@ echo "Creating kafka namespace"
 kubectl create namespace kafka >/dev/null 2>&1
 echo "creating kafka cluster"
 kubectl apply -f ./config/namespace.yaml -n kafka >/dev/null 2>&1
+echo "waiting from the strimzi operator to be ready"
+kubectl wait --for=condition=ready pod -l name=strimzi-cluster-operator -n kafka --timeout=300s
 kubectl apply -f ./config/kafka.yaml -n kafka >/dev/null 2>&1
-echo "waiting for kafka to be ready"
-kubectl wait --for=condition=ready pod -l app=kafka -n kafka --timeout=300s
+echo "waiting for the zookeeper cluster to be ready"
+kubectl wait --for=condition=ready pod -l app=my-cluster-zookeeper -n kafka --timeout=300s
+echo "waiting for the kafka cluster to be ready"
+kubectl wait --for=condition=ready pod -l app=my-cluster-kafka -n kafka --timeout=300s
 echo "creating kafka topics"
 kubectl apply -f ./config/topic.yaml -n kafka >/dev/null 2>&1
 echo "deploying kafka connect"
