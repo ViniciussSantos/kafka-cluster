c54f9fd56316026d45de7f1fe82fe94f84ab845b
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-02-12 15:46:22 -0300

    change mqtt connector plugin to Lenses.io

--- scripts/configure-connect.sh
+++ scripts/configure-connect.sh
@@ -4,20 +4,18 @@ node_ip=$(kubectl get node worker01 -o=jsonpath='{range .status.addresses[?(@.ty
 
 service_port=$(kubectl get service kafka-connect-port -o=jsonpath='{.spec.ports[0].nodePort}{"\n"}' -n kafka)
 
-curl -X POST \
-	"$node_ip:$service_port/connectors" \
-	-H 'Content-Type: application/json' \
-	-d '{
-    "name": "mqtt-source-connector",
-    "config": {
-      "connector.class": "be.jovacon.kafka.connect.MQTTSourceConnector",
-      "mqtt.topic": "my-topic",
-      "kafka.topic": "my-topic",
-      "mqtt.clientID": "my_client_id",
-      "mqtt.broker": "tcp://192.168.15.4:1883",
-      "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-      "key.converter.schemas.enable": "false",
-      "value.converter": "org.apache.kafka.connect.storage.StringConverter",
-      "value.converter.schemas.enable": "false"
-    }
-  }'
+curl "$node_ip:$service_port/connectors" -X POST -H 'Content-Type: application/json' -d '{
+ "name":"mqtt-source-connector",
+ "config":{
+ "connector.class":"io.lenses.streamreactor.connect.mqtt.source.MqttSourceConnector",
+ "connect.mqtt.hosts":"tcp://192.168.60.1:1883",
+ "connect.mqtt.clean":"true",
+ "connect.mqtt.timeout":"1000",
+ "connect.mqtt.keep.alive":"1000",
+ "connect.mqtt.service.quality":"1",
+ "connect.mqtt.kcql":"INSERT INTO test-topic SELECT * FROM test/topic",
+ "connect.progress.enabled":"true",
+ "connect.mqtt.process.duplicates":"true",
+ "connect.mqtt.log.message":"true"
+ }
+}' | jq
