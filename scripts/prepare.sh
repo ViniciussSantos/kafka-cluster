d78c1e2cdd306ebb108ceec4a10ccfa5d29332b8
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-07-07 20:48:04 -0300

    update kubectl/kubeadm/kubelet install script to working conditions

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,12 +3,14 @@
 nodetype="$1"
 
 echo "[Step 1- Installing required components]"
-sudo apt update >/dev/null 2>&1
-sudo apt install -y curl >/dev/null 2>&1
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - >/dev/null 2>&1
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
-sudo apt install -y kubeadm=1.22.10-00 kubelet=1.22.10-00 kubectl=1.22.10-00 >/dev/null 2>&1
-sudo apt-mark hold kubeadm kubelet kubectl >/dev/null 2>&1
+sudo apt-get update >/dev/null 2>&1
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg >/dev/null 2>&1
+sudo mkdir -p -m 755 /etc/apt/keyrings >/dev/null 2>&1
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg >/dev/null 2>&1
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list >/dev/null 2>&1
+sudo apt-get update >/dev/null 2>&1
+sudo apt-get install -y kubelet kubeadm kubectl >/dev/null 2>&1
+sudo apt-mark hold kubelet kubeadm kubectl >/dev/null 2>&1
 echo "[>- Install and configure Docker]"
 sudo apt install -y docker.io >/dev/null 2>&1
 sudo systemctl enable docker >/dev/null 2>&1
@@ -46,24 +48,24 @@ cat >>/etc/hosts <<EOF
 EOF
 
 if [ $1 == "master" ]; then
-	echo "[Step 2 - Initializing Master Node]"
-	sudo kubeadm init --apiserver-advertise-address 192.168.60.11 --control-plane-endpoint 192.168.60.11 --pod-network-cidr=10.244.0.0/16 >/dev/null 2>&1
-	echo "[>- Installing Kubernetes network plugin]"
-	echo "[>- Enable ssh password authentication]"
-	sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-	echo 'PermitRootLogin yes' >>/etc/ssh/sshd_config
-	systemctl reload sshd
-	echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
-	kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
-	kubeadm token create --print-join-command >/joincluster.sh 2>/dev/null
-	echo -e "---------COPY AND PASTE THE FOLLOWING IN ~/.kube/config ON THE MACHINE YOU WANT TO RUN KUBECTL COMMANDS-------\n"
-	cat /etc/kubernetes/admin.conf
-	echo -e "\n---------------------"
+  echo "[Step 2 - Initializing Master Node]"
+  sudo kubeadm init --apiserver-advertise-address 192.168.60.11 --control-plane-endpoint 192.168.60.11 --pod-network-cidr=10.244.0.0/16 >/dev/null 2>&1
+  echo "[>- Installing Kubernetes network plugin]"
+  echo "[>- Enable ssh password authentication]"
+  sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+  echo 'PermitRootLogin yes' >>/etc/ssh/sshd_config
+  systemctl reload sshd
+  echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+  kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+  kubeadm token create --print-join-command >/joincluster.sh 2>/dev/null
+  echo -e "---------COPY AND PASTE THE FOLLOWING IN ~/.kube/config ON THE MACHINE YOU WANT TO RUN KUBECTL COMMANDS-------\n"
+  cat /etc/kubernetes/admin.conf
+  echo -e "\n---------------------"
 fi
 
 if [ $1 == "worker" ]; then
-	echo "[Join worker to cluster]"
-	apt install -qq -y sshpass >/dev/null 2>&1
-	sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no master01.kubernetes.cluster:/joincluster.sh /joincluster.sh 2>/dev/null
-	bash /joincluster.sh >/dev/null 2>&1
+  echo "[Join worker to cluster]"
+  apt install -qq -y sshpass >/dev/null 2>&1
+  sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no master01.kubernetes.cluster:/joincluster.sh /joincluster.sh 2>/dev/null
+  bash /joincluster.sh >/dev/null 2>&1
 fi
